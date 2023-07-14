b7c2e22b97787c1b339f93d9ddb7a720381b2b0c
Fachrul Ari <fhrlarfn@gmail.com>
2023-07-14 21:43:04 +0700

    Add roman string into numeral function

--- Roman Numeral Converter/index.js
+++ Roman Numeral Converter/index.js
@@ -37,3 +37,49 @@ function convertToRoman(num) {
 }
 
 convertToRoman(36);
+
+console.log("==== Break ====");
+
+// Roman To Numeral
+var romanToInt = function (s) {
+  // Create Roman Numeral Object
+  const sym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  // Explanation of object with square bracket notation "[]" means contain the key of object as string
+  // We cannot use dot notation "." to access a property whose name is not a valid JavaScript identifier
+  // And if string had square bracket  means individual characters correspond to a numerical index
+  // Example: Object["String Key"[index of the char]]
+  // Create accumulator for total
+  let accumulator = 0;
+  // Loop based on how long string is
+  for (let i = 0; i < s.length; i++) {
+    const cur = sym[s[i]];
+    const next = sym[s[i + 1]];
+    // If the current index of string is lower than next string.
+    // The "next" value will substract with "cur" Example:"IX" (10 - 1) and add into accumulator
+    if (cur < next) {
+      accumulator += next - cur;
+      // +1 on loop index since we already add the NEXT ROMAN and substract it with current
+      i++;
+    }
+    // If not lower than next string, just add current loop string index ("cur")
+    else {
+      accumulator += cur;
+    }
+    // Or we can simplify inside the loop into this
+    //   sym[s[i]] < sym[s[i + 1]]
+    //     ? (accumulator -= sym[s[i]])
+    //     : (accumulator += sym[s[i]]);
+  }
+  console.log(accumulator);
+  return accumulator;
+};
+
+romanToInt("MCMXCIV");
