cfe5646833a7663686216b561173aadf95d490c6
Fachrul Ari <fhrlarfn@gmail.com>
2023-07-15 20:15:12 +0700

    Prefix

---
+++ LeetCode Challange/Prefix.js
@@ -0,0 +1,18 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+
+  for (let i = 0; i <= strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+  return strs[0];
+};
+
+longestCommonPrefix(["flower", "flow", "flight"]);
