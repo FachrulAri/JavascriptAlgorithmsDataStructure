d738e00b29d9796f3c7b1f6b67a1a4337d2a588a
Fachrul Ari <fhrlarfn@gmail.com>
2023-07-16 18:34:19 +0700

    merge 2 list

--- LeetCode Challange/Prefix.js
+++ LeetCode Challange/Prefix.js
@@ -4,7 +4,6 @@
 // Output: "fl"
 var longestCommonPrefix = function (strs) {
   if (strs.length === 0) return "";
-
   for (let i = 0; i <= strs[0].length; i++) {
     for (let j = 1; j < strs.length; j++) {
       if (strs[0][i] !== strs[j][i]) {
