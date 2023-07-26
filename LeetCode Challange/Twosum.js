26a17f567dbf67344ddc709ab84c67d9580064fa
Fachrul Ari <fhrlarfn@gmail.com>
2023-07-26 19:29:25 +0700

    plusone on array of number

--- LeetCode Challange/Twosum.js
+++ LeetCode Challange/Twosum.js
@@ -41,3 +41,31 @@ var twoSumHash = function (nums, target) {
   return [];
 };
 twoSumHash([2, 7, 11, 15], 17);
+
+console.log("==== Break ====");
+// You are given a large integer represented as an integer array digits,
+// Where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order.
+// The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321. Incrementing by one gives 4321 + 1 = 4322. Thus, the result should be [4,3,2,2].
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9. Incrementing by one gives 9 + 1 = 10. Thus, the result should be [1,0].
+var plusOne = function (digits) {
+  const num = BigInt(digits.join("")) + 1n;
+  const numToString = num.toString();
+
+  let arr = [];
+
+  for (let i = 0; i < numToString.length; i++) {
+    const num = parseInt(numToString[i]);
+    arr.push(num);
+  }
+  return arr;
+};
+console.log(plusOne([4, 3, 2, 9]));
